5f5e25b98a7e3370016e635b3955b71547ee05be
Mirek Behina <mirekbehina@seznam.cz>
2018-12-31 18:40:09 +0100

    Update source.js

--- source.js
+++ source.js
@@ -1,29 +1,43 @@
 
 $( document ).ready(function() {
 
-  
+    function makeid() {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      
+        for (var i = 0; i < 5; i++)
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+      
+        return text;
+      }
+      
+      console.log(makeid());
+
+  /*
     var server = "https://api.openweathermap.org/data/2.5/forecast?q="
     var api_klic = "&APPID=afb6e76426a0802ed7f8dcdb42900eab&units=metric"
 
 
-    $('input').submit(function(e){
-        e.preventDefault();
-        input = $('#mesto').val(); //vice hashtagu
-
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": server + mesto + api_klic,
-            "method": "GET",
-            error: function(e) {
-                console.log("selhani");
-            }
-          }
-
-
-        $.ajax(settings).done(function (response) {
-
-        });
-
+    $.ajax({
+        url: "https://api.twitter.com/1.1/tweets/search/30day/production.json",
+        beforeSend: function(xhr) { 
+          xhr.setRequestHeader("Authorization", "Basic " + btoa("username:password")); 
+        },
+        type: 'POST',
+        dataType: 'json',
+        contentType: 'application/json',
+        processData: false,
+        data: '{"query":"from:TwitterDev lang:en",
+                "maxResults": "100",
+                "fromDate":"<YYYYMMDDHHmm>", 
+                "toDate":"<YYYYMMDDHHmm>"
+            }',
+        success: function (data) {
+          alert(JSON.stringify(data));
+        },
+        error: function(){
+          alert("Cannot get data");
+        }
     });
+*/
 });
