23f5158b276f91489866bc50a810ec962cb58977
Mirek Behina <mirekbehina@seznam.cz>
2018-12-31 18:41:26 +0100

    Update source.js

--- source.js
+++ source.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
         var text = "";
         var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
       
-        for (var i = 0; i < 5; i++)
+        for (var i = 0; i < 42; i++)
           text += possible.charAt(Math.floor(Math.random() * possible.length));
       
         return text;
